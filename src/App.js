7a876c9dd34043ab002d45814416c78f60b5137b
adarshsingh2k <adarshsingh2k@gmail.com>
2020-05-23 00:57:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './App.css';
+import Game from './components/Game';
+
+class App extends React.Component{
+  render(){
+    return(
+     
+      <Game/>
+    )
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello world!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+export default App;
